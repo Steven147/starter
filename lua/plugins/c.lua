9197b5f7a5962a929639d843eb5b7035e156bb8f
lsq <lsq@mba14.m2>
2026-05-13 15:14:44 +0800

    feat(c): add lldb-dap debugging
    Use Xcode's lldb-dap and skip Mason's codelldb auto-install so C/C++ debugging works reliably in this macOS LazyVim setup.

--- lua/plugins/c.lua
+++ lua/plugins/c.lua
@@ -17,4 +17,65 @@ return {
       vim.list_extend(opts.ensure_installed, { "c", "cpp" })
     end,
   },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.automatic_installation = opts.automatic_installation or {}
+
+      if opts.automatic_installation == true then
+        opts.automatic_installation = { exclude = { "codelldb" } }
+        return
+      end
+
+      local exclude = opts.automatic_installation.exclude or {}
+      if not vim.tbl_contains(exclude, "codelldb") then
+        table.insert(exclude, "codelldb")
+      end
+      opts.automatic_installation.exclude = exclude
+    end,
+  },
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    opts = function()
+      local dap = require("dap")
+      local lldb_dap = vim.fn.exepath("lldb-dap")
+
+      if lldb_dap == "" then
+        lldb_dap = "/Applications/Xcode.app/Contents/Developer/usr/bin/lldb-dap"
+      end
+
+      dap.adapters.codelldb = {
+        type = "executable",
+        command = lldb_dap,
+        name = "lldb",
+      }
+
+      if vim.fn.filereadable(lldb_dap) == 0 then
+        vim.notify("lldb-dap not found: " .. lldb_dap, vim.log.levels.WARN)
+      end
+
+      for _, lang in ipairs({ "c", "cpp" }) do
+        dap.configurations[lang] = {
+          {
+            type = "codelldb",
+            request = "launch",
+            name = "Launch file",
+            program = function()
+              return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+            end,
+            cwd = "${workspaceFolder}",
+          },
+          {
+            type = "codelldb",
+            request = "attach",
+            name = "Attach to process",
+            pid = require("dap.utils").pick_process,
+            cwd = "${workspaceFolder}",
+          },
+        }
+      end
+    end,
+  },
 }
